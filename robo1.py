ca2d7ec401b65c1d2dff7b34e4625296f4234748
Luigi <luiggimartins8@gmail.com>
2025-10-04 08:34:55 -0300

    Automate domain search for multiple domains
    Refactored the script to iterate over a list of domains and perform the search for each one on registro.br. The results for each domain are printed, improving automation and scalability.

--- robo1.py
+++ robo1.py
@@ -18,21 +18,20 @@ driver = webdriver.Chrome(service=service, options=options)
 # Abre o site
 driver.get("https://registro.br/")
 
-# ✅ Localiza a barra de pesquisa pelo ID
-# No site registro.br, o campo tem id="is-avail-field"
-pesquisa = driver.find_element(By.ID, "is-avail-field")
-# Limpa o campo antes de digitar
-pesquisa.clear()
-dominio = "roboscompython.com.br"
-# Digita o domínio desejado
-pesquisa.send_keys(dominio)
-# Pressiona ENTER para pesquisar
-pesquisa.send_keys(Keys.RETURN)
-time.sleep(2)
+dominios = ["roboscompython.com.br" , "udemy.com" , "uol.com.br" , "pythoncurso.com"] #vai pesquisar uma lista
+for dominio in dominios:
+    # ✅ Localiza a barra de pesquisa pelo ID
+    # No site registro.br, o campo tem id="is-avail-field"
+    pesquisa = driver.find_element(By.ID, "is-avail-field")
+    pesquisa.clear() # Limpa o campo antes de digitar# ✅ Localiza a barra de pesquisa pelo ID
+    # No site registro.br, o campo tem id="is-avail-field"
+    pesquisa = driver.find_element(By.ID, "is-avail-field")
+    pesquisa.clear() # Limpa o campo antes de digitar
+    pesquisa.send_keys(dominio) # Digita o domínio desejado
+    pesquisa.send_keys(Keys.RETURN) # Pressiona ENTER para pesquisar
+    time.sleep(2)
+    resultados = driver.find_elements(By.TAG_NAME, "strong")
+    print("Domínio %s %s" % (dominio, resultados[2].text))
 
-resultados = driver.find_elements(By.TAG_NAME, "strong")
-print("Domínio %s %s" % (dominio, resultados[2].text))
-
-time.sleep(8)
-# Fecha o navegador
-driver.close()
+time.sleep(3)
+driver.close() # Fecha o navegador
